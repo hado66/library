c174a95975751e3538c7ee86b357c8b277b640ab
hado66 <632579797@qq.com>
2020-10-02 15:01:07 +0800

    创建.gitignore

--- application/views.py
+++ application/views.py
@@ -7,13 +7,13 @@ from django.db import connection
 def test(request):
     models.Test.objects.create(name='title')
     data = MVC_HOLDER.services["test"].get_model({"name": "test"})
-
-    # print(MVC_HOLDER.services["test"].get_count())
     print(type(data))
     data = "动态的后端数据"
     cursor = connection.cursor()
     print(cursor.execute("select table_name from information_schema.tables where table_schema='library'"))
-    print(cursor.fetchall())
+    x = cursor.fetchall()
+    for i in x:
+        print(i[0])
 
 
     return render(request, "test/index.html", {"data": data})
